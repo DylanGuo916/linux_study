e32d9c5f70f9239b2626238f0f690496096ff99e
guozhe <dylanguo916@gmail.com>
2021-08-06 00:11:12 +0800

    Create deadlock.c

---
+++ thread/deadlock.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <pthread.h>
+#include <unistd.h>
+
+pthread_mutex_t mutex1;
+pthread_mutex_t mutex2;
+
+void* workA(void* arg) {
+  pthread_mutex_lock(&mutex1);
+  sleep(1);
+  pthread_mutex_lock(&mutex2);
+
+  printf("workA...\n");
+
+  pthread_mutex_unlock(&mutex2);
+  pthread_mutex_unlock(&mutex1);
+  return NULL;
+}
+
+void *workB(void *arg) {
+  pthread_mutex_lock(&mutex2);
+  sleep(1);
+  pthread_mutex_lock(&mutex1);
+
+  printf("workB...\n");
+
+  pthread_mutex_unlock(&mutex1);
+  pthread_mutex_unlock(&mutex2);
+  return NULL;
+}
+
+int main() {
+  pthread_mutex_init(&mutex1, NULL);
+  pthread_mutex_init(&mutex2, NULL);
+
+  pthread_t tid1, tid2;
+  pthread_create(&tid1, NULL, workA, NULL);
+  pthread_create(&tid2, NULL, workB, NULL);
+
+  pthread_join(tid1, NULL);
+  pthread_join(tid2, NULL);
+
+  pthread_mutex_destroy(&mutex1);
+  pthread_mutex_destroy(&mutex2);
+}
